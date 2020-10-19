bbaf56ac0e4be7a5ca6e53ec172e83bc04c97464
coriander31415 <coriander31415@gmail.com>
2020-10-20 02:21:28 +0300

    feat: solve human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports =
+    function toReadable(num) {
+        let res = '';
+        let nums = [
+            'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+            'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',
+            'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+        ];
+
+        // zero
+        if (num === 0) {
+            res = nums[0];
+        }
+
+        // hundreds
+        if (num >= 100) {
+            res = nums[Math.floor(num / 100)] + " hundred";
+            num %= 100;
+        }
+
+        // tens
+        if (num > 20 && num !== 0) {
+            if (res) res += ' ';
+            res += nums[Math.floor(num / 10 + 18)];
+            num %= 10;
+        }
+
+        // units
+        if (num <= 20 && num !== 0) {
+            if (res) res += ' ';
+            res += nums[num];
+        }
+
+        return res;
+    }
